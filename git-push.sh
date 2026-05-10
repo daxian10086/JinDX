12c7cdab010fcd606899079cf5ce4ab23c1f9bfb
daxian10086 <wd163mms123@163.com>
2026-05-10 14:07:24 +0800

    Update git-push.sh to remove default token
    Remove default GitHub token for security reasons.

--- git-push.sh
+++ git-push.sh
@@ -5,7 +5,7 @@
 set -e
 cd "$(dirname "$0")"
 
-TOKEN="${GITHUB_TOKEN:-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}"
+TOKEN="${GITHUB_TOKEN:-}"
 REPO="daxian10086/JinDX"
 API_IP="140.82.113.5"
 BRANCH="master"
